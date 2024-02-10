d06ef4d38d1371e7c9277a2dc82bd81c1b2ecfb7
frozenopertor <adityagupta11112002@gmail.com>
2024-02-10 15:49:57 +0530

    Earth modified

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,51 +1,44 @@
-import React from 'react'
-import { Suspense } from 'react'
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
-import CanvasLoader from '../Loader'
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
-
-
+import CanvasLoader from "../Loader";
 
 const Earth = () => {
-  const earth = useGLTF('./planet/scene.gltf')
+  const earth = useGLTF("./planet/scene.gltf");
+
   return (
-    <primitive
-      object = {earth.scene}
-      scale= {2.5}
-      position-y = {0}
-      rotation-y = {0}
-    />
-  )
-}
+    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
+  );
+};
 
 const EarthCanvas = () => {
   return (
     <Canvas
       shadows
       frameloop='demand'
-      gl = {{preserveDrawingBuffer: true}}
+      dpr={[1, 2]}
+      gl={{ preserveDrawingBuffer: true }}
       camera={{
-        fov:45,
+        fov: 45,
         near: 0.1,
         far: 200,
-        position: [-4,3,6]
+        position: [-4, 3, 6],
       }}
     >
-      <Suspense fallback = {<CanvasLoader/>}>
+      <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
-          autoRotate = {true}
-          autoRotateSpeed={10}
-          enableZoom = {false}
-          maxPolarAngle={Math.PI/2}
-          minPolarAngle={Math.PI/2}
+          autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
         />
+        <Earth />
 
-        <Earth/>
+        <Preload all />
       </Suspense>
     </Canvas>
-    
-  )
-}
+  );
+};
 
-export default EarthCanvas
+export default EarthCanvas;

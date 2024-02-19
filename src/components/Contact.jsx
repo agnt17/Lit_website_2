74208bca2a9cb919f6d92ba89acd63ba7427984a
frozenopertor <adityagupta11112002@gmail.com>
2024-02-19 22:17:02 +0530

    email js

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -47,9 +47,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "Aditya",
+          to_name: "Literary Sub-Council",
           from_email: form.email,
-          to_email: "adityagupta20042003@gmail.com",
+          to_email: "literarysubcouncil@hbtu.ac.in",
           message: messageWithSenderEmail, // Use the modified message
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY

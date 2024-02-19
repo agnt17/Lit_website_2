7d3d63036ed14543d4ee91f4edf7f06a5b0d8217
frozenopertor <adityagupta11112002@gmail.com>
2024-02-19 22:26:04 +0530

    new

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -47,7 +47,7 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "Literary Sub-Council",
+          to_name: "LiterarySub-Council",
           from_email: form.email,
           to_email: "literarysubcouncil@hbtu.ac.in",
           message: messageWithSenderEmail, // Use the modified message

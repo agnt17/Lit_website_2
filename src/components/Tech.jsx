5159c33b6a6aff80c392e4c0ba6397184f890869
frozenopertor <adityagupta11112002@gmail.com>
2024-02-19 19:18:53 +0530

    magazine section updated

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -9,20 +9,7 @@ function Tech() {
       <h2 className={styles.sectionHeadText}>
         <span className="select-none">Magazine</span>
       </h2>
-      <div className="flex items-center justify-center p-4">
-        <div className="flex flex-col lg:flex-row bg-white bg-opacity-5 backdrop-blur-md rounded-lg text-center w-full md:w-5/6 lg:w-4/6 lg:h-auto sm:h-auto p-4 relative">
-          <div className="flex-1 pb-4 lg:pb-0">
-            {/* Top content */}
-            Content
-          </div>
-          <div className="border-t border-white border-opacity-50 w-full lg:w-0 lg:border-none"></div>
-          <div className="flex-1 pt-4">
-            {/* Bottom content */}
-            
-            Content
-          </div>
-        </div>
-      </div>
+      <p className="text-xl text-teal-500 font-extralight">Coming Soon...</p>
     </>
   );
 }

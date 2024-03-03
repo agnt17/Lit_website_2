8e12bea5649ea621a513aaadcfb5d5431d8accc0
frozenopertor <adityagupta11112002@gmail.com>
2024-03-03 12:08:48 +0530

    location added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 // import "bootstrap/dist/css/bootstrap.css"
 import { FaInstagram } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
-import { FaXTwitter } from "react-icons/fa6";
+import { FaLocationDot, FaXTwitter } from "react-icons/fa6";
 
 function Footer() {
   return (
@@ -15,12 +15,20 @@ function Footer() {
         </div>
         <div className="flex flex-row ml-4 pr-10 mt-2">
           <ul className="flex flex-row ">
-            <li className="flex ml-4 mr-4 text-3xl text-gray-400">
+            <li className="flex  flex-rowml-4 mr-4 text-3xl text-gray-400 ">
               <a
                 href="https://www.instagram.com/literarysubcouncil_hbtu/"
                 target="_blank"
               >
                 <FaInstagram className="hover:text-pink-700" />
+              </a>
+              <a
+                href="https://www.google.com/maps/place/HBTU+-+Kanpur/@26.4934722,80.3048467,17z/data=!3m1!4b1!4m6!3m5!1s0x399c472bc6fb5a61:0x12b6fef2872cee80!8m2!3d26.4934674!4d80.3074216!16s%2Fg%2F11cn5pgp2z?entry=ttu"
+                className="ml-4"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaLocationDot className="hover:text-red-600" />
               </a>
             </li>
           </ul>

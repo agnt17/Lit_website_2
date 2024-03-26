2d724732dccd565ca64b7923ef52b32eb5e8b49d
frozenopertor <adityagupta11112002@gmail.com>
2024-03-26 23:49:04 +0530

    2 events added

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -164,26 +164,57 @@ const Feedbacks = () => {
           ))}
         </VerticalTimeline>
       </div>
-      <div className="items-center">
-        <p className="m-5 font-medium text-xl flex mx-auto ml-5 text-center justify-center text-teal-500">
-          For registration of other colleges
+      <div className="mt-20 ml-5">
+        <p className={styles.sectionHeadText}>Registration</p>
+        <p className={styles.sectionSubText}>
+          How to get into such Amazing Events....
         </p>
-        <p className="m-5 font-medium text-md flex mx-auto ml-5 text-center justify-center text-teal-500">
-          You can register for these events right here by just filling this
-          Google Form
-        </p>
-        <button className="flex justify-center items-center mt-10 mb-[-50px] font-semibold text-2xl hover:text-black cursor-pointer bg-violet-600 hover:bg-violet-400 rounded-lg w-fit p-4  mx-auto ">
-          <a
-            href="https://docs.google.com/forms/d/1mtFtuKlrlvpu4ZTa9ekXwq12zs0hqTIyT3CPRiWB3-k/edit?ts=65f05eb6"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Google form link
-          </a>
-        </button>
+      </div>
+      <div className="ml-5">
+        <table className="mt-5 w-full " style={{ borderCollapse: "collapse" }}>
+          <tr>
+            <td style={cellStyle} className="text-teal-400">
+              Registration for{" "}
+              <span className="text-fuchsia-500 text-xl">HBTU</span> Students...
+            </td>
+            <td style={cellStyle} className="text-teal-400">
+              Registration for{" "}
+              <span className="text-fuchsia-500 text-xl">Other</span>{" "}
+              Students...
+            </td>
+          </tr>
+          <tr>
+            <td style={cellStyle}>
+              <a
+                href="https://docs.google.com/forms/d/1YfcoD3sUm8mn6uzVndNwf1LmnYvAc8TvXxz8sQg8fH0/edit?ts=65fd39db"
+                target="_blank"
+                className="hover:text-blue-600"
+              >
+                <button className="bg-purple-500 p-2 rounded-lg hover:bg-purple-800 text-white">
+                  Google form Link
+                </button>
+              </a>
+            </td>
+            <td style={cellStyle} className="" target="_blank">
+              <a
+                href="https://docs.google.com/forms/d/1mtFtuKlrlvpu4ZTa9ekXwq12zs0hqTIyT3CPRiWB3-k/edit?ts=65f05eb6"
+                target="_blank"
+                className="hover:text-blue-600"
+              >
+                <button className="bg-purple-500 p-2 rounded-lg hover:bg-purple-800 text-white">
+                  Google form Link
+                </button>
+              </a>
+            </td>
+          </tr>
+        </table>
       </div>
     </>
   );
+};
+const cellStyle = {
+  border: "1px solid #ddd",
+  padding: "8px",
 };
 
 export default SectionWrapper(Feedbacks, "events");
